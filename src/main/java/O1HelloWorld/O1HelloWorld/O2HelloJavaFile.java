b0c5879b23b30248c170da9d1556962de101f372
morganlinn <to.morganlinn@gmail.com>
2023-09-22 19:31:53 +0200

    O6CompilationCatchingErrors

--- src/main/java/O1HelloWorld/O1HelloWorld/O2HelloJavaFile.java
+++ src/main/java/O1HelloWorld/O1HelloWorld/O2HelloJavaFile.java
@@ -4,7 +4,7 @@ public class O2HelloJavaFile {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello Morgan!");
+        System.out.println("Hello, Morgan!");
 
     }
 
--- src/main/java/O1HelloWorld/O1HelloWorld/O6CompilationCatchingErrors.java
+++ src/main/java/O1HelloWorld/O1HelloWorld/O6CompilationCatchingErrors.java
@@ -1,4 +1,16 @@
 package O1HelloWorld.O1HelloWorld;
 
 public class O6CompilationCatchingErrors {
+
+        public static void main(String[] args) {
+
+            System.out.println("Java is a class-based language.");
+            System.out.println("Java classes have a 'main' method.");
+            System.out.println("Java statements end with a semicolon.")
+
+            System.out.println("Programming is... fun!");
+
+    }
 }
+
+// Type javac O6CompilationCatchingErrors.java in the terminal
